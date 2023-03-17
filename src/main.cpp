17c491bbd5221dd94980a842398f6cf68dd43c32
Th3UwU <angel.storres@alumnos.udg.mx>
2023-03-17 10:35:40 -0600

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,113 @@
+#include <cstdio>
+#include <cstdlib>
+#include <cstdint>
+
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+using namespace glm;
+
+extern "C"
+{
+	#include <sndfile.h>
+	#include <GLFW/glfw3.h>
+	#include <GL/glew.h>
+}
+
+#include <shader.hpp>
+
+void readSound(const char* path, int16_t*& pcm, SF_INFO& sndInfo)
+{
+	SNDFILE* sndFile = sf_open(path, SFM_READ, &sndInfo);
+	pcm = (int16_t*)malloc(sizeof(int16_t) * sndInfo.channels * sndInfo.frames);
+	sf_count_t framesRead = sf_readf_short(sndFile, pcm, sndInfo.frames);
+	if (framesRead == sndInfo.frames)
+	{
+		printf("Sucess\n");
+		printf("Total frames: %lli\n", sndInfo.frames);
+	}
+	sf_close(sndFile);
+}
+
+int main()
+{
+	int16_t* pcm;
+	SF_INFO sndInfo;
+	readSound("../song.wav", pcm, sndInfo);
+
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_RESIZABLE, false);
+
+	GLFWwindow* window = glfwCreateWindow(1280, 720, "Audio Plot", NULL, NULL);
+	glfwMakeContextCurrent(window);
+	glewInit();
+
+	glViewport(0, 0, 1280, 720);
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+	mat4 projection = ortho(0.0f, 1280.0f, 720.0f, 0.0f, -1.0f, 1.0f);
+	Shader shaderLines("../shader/lineStrip.vert", "../shader/lineStrip.frag");
+
+	// Get points
+	// Allocate the double size of PCM (X, Y)
+	//  x1   y1     x2   y2     x3   y3
+	// [0][pcm[0]] [1][pcm[1]] [2][pcm[2]] ...
+	int16_t* wave = (int16_t*)malloc(sizeof(int16_t) * sndInfo.frames * 2);
+	for (sf_count_t i = 0; i < sndInfo.frames; i++)
+	{
+		wave[i*2] = i;				// X
+		wave[(i*2)+1] = -pcm[i];	// Y
+	}
+	free(pcm);
+
+	// Wave VAO
+	GLuint VAO, VBO;
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(int16_t) * sndInfo.frames * 2, wave, GL_STATIC_DRAW);
+
+	glBindVertexArray(VAO);
+	glEnableVertexAttribArray(0);
+	glVertexAttribIPointer(0, 2, GL_SHORT, 2 * sizeof(int16_t), (void*)(sizeof(int16_t) * 0));
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwPollEvents();
+
+		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		// DRAW WAVE
+
+		/* Transformations */
+		mat4 model = mat4(1.0f);
+		model = translate(model, vec3(0.0f, 360.0f, 0.0f));
+		model = scale(model, vec3(0.04f, 0.005f, 0.0f));
+
+		/* Set shader and pass transformations */
+		shaderLines.use();
+		shaderLines.setValue("model", model);
+		shaderLines.setValue("projection", projection);
+
+		glBindVertexArray(VAO);
+		glDrawArrays(GL_LINE_STRIP, 0, sndInfo.frames);
+		glBindVertexArray(0);
+		//
+
+		glfwSwapBuffers(window);
+	}
+
+	glfwTerminate();
+	free(wave);
+
+	return 0;
+}
